2b720d9b8dc8c61999d5b46476bb7be13f4fa6ea
Pradeepta Kumar Say <pradeepta46@gmail.com>
2021-07-02 22:19:23 +0530

    small change

--- src/test/java/Core/TakeScreenshot.java
+++ src/test/java/Core/TakeScreenshot.java
@@ -13,7 +13,7 @@ public class TakeScreenshot extends StartingChromeBrowser{
 		File scrShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		try {
 			FileUtils.copyFile(scrShot, new File(path));
-			System.out.println("Screenshot saved in the Path "+path);
+			System.out.println("Screenshot saved in the Path ="+path);
 		} catch (IOException e) {			
 			e.printStackTrace();
 		}
